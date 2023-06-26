5ae4ee304a610ab0aeb1e9bcdc302a582e3e5670
RizaMamedow <riza.mamedow22@gmail.com>
2023-06-26 12:46:37 +0500

    some change #3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import './globals.css';
-import {AppHeader, ContentBox, Navigation} from "../components";
+import {AppHeader, Navigation} from "../components";
 
 
 export default function App({ Component, pageProps: { session, ...pageProps }}) {
