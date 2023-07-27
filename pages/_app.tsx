868573056ded3fcc3b9a986649a2191120919291
RizaMamedow <riza.mamedow22@gmail.com>
2023-07-27 13:19:51 +0500

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,28 +4,63 @@ import {AppHeader, Navigation} from "../components";
 import {NextIntlClientProvider} from "next-intl";
 import {LocaleSwitcher} from "../components";
 import {SiVercel, SiNextdotjs} from "react-icons/si";
+import Script from "next/script"
+import * as gtag from "../gtag"
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 // @ts-ignore
 export default function App({ Component, pageProps: { ...pageProps }}) {
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            gtag.pageview(url)
+        }
+        router.events.on("routeChangeComplete", handleRouteChange)
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange)
+        }
+    }, [])
     return (
-        <NextIntlClientProvider messages={pageProps.messages}>
-            <div className='pt-5 md:pt-10 min-h-screen px-15 lg:px-52 pb-10'>
-                <div className="flex align-center flex-col">
-                    <AppHeader/>
-                    <div className="m-2 flex justify-end flex-row">
-                        <LocaleSwitcher />
-                    </div>
-                    <div className="mt-5 md:mt-15 container mx-auto grid grid-cols-1 md:grid-cols-3 gap-1">
-                        <Navigation/>
-                        <div className="col-span-2 p-5 pt-0">
-                            <Component {...pageProps} />
+        <>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+            />
+            <NextIntlClientProvider messages={pageProps.messages}>
+                <div className='pt-5 md:pt-10 min-h-screen px-15 lg:px-52 pb-10'>
+                    <div className="flex align-center flex-col">
+                        <AppHeader/>
+                        <div className="m-2 flex justify-end flex-row">
+                            <LocaleSwitcher />
+                        </div>
+                        <div className="mt-5 md:mt-15 container mx-auto grid grid-cols-1 md:grid-cols-3 gap-1">
+                            <Navigation/>
+                            <div className="col-span-2 p-5 pt-0">
+                                <Component {...pageProps} />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="m-6 text-white text-sm italic flex justify-center content-center">
-                Built with <SiNextdotjs className="mx-1 mt-0.5"/> Next.js, deployed with <SiVercel className="mx-1 mt-0.5"/> Vercel
-            </div>
-        </NextIntlClientProvider>
+                <div className="m-6 text-white text-sm italic flex justify-center content-center">
+                    Built with <SiNextdotjs className="mx-1 mt-0.5"/> Next.js, deployed with <SiVercel className="mx-1 mt-0.5"/> Vercel
+                </div>
+            </NextIntlClientProvider>
+        </>
+
     );
 };
