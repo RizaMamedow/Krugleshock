7f26dda739f2981b154062470604eb13fa346cfa
RizaMamedow <riza.mamedow22@gmail.com>
2023-07-27 13:43:30 +0500

    Added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,13 +18,15 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
-                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-6KNDXGB249"></Script>
-                <Script>
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'G-6KNDXGB249');
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-6KNDXGB249" />
+                <Script id="google-analytics">
+                    {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+             
+                      gtag('config', 'G-6KNDXGB249');
+                    `}
                 </Script>
             </body>
         </Html>
