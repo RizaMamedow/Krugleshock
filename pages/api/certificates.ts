baa2185f116552a133089d0e5638b11ca0ce7bca
RizaMamedow <riza.mamedow22@gmail.com>
2023-08-03 18:07:34 +0500

    added vercel analitycs and removed google analitycs

--- pages/api/certificates.ts
+++ pages/api/certificates.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const dataFilePath = path.join(process.cwd(), 'certificates.json');
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
     if (req.method === 'GET') {
         const jsonData = await fsPromises.readFile(dataFilePath);
         const objectData = JSON.parse(jsonData);
