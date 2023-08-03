66eaed905af5e43375d3e2c4fcf9dc75479a60b9
RizaMamedow <riza.mamedow22@gmail.com>
2023-08-03 18:11:15 +0500

    fixes

--- pages/api/certificates.ts
+++ pages/api/certificates.ts
@@ -6,7 +6,7 @@ const dataFilePath = path.join(process.cwd(), 'certificates.json');
 export default async function handler(req: any, res: any) {
     if (req.method === 'GET') {
         const jsonData = await fsPromises.readFile(dataFilePath);
-        const objectData = JSON.parse(jsonData);
+        const objectData = JSON.parse(jsonData.toString());
 
         res.status(200).json(objectData);
     }
