86e1d950f02813490abf756a073e75fa6348ec53
RizaMamedow <riza.mamedow22@gmail.com>
2024-01-31 18:11:03 +0500

    SEO Commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import * as React from "react";
 import Script from "next/script";
 
+
+
 export default function Document() {
+    let keywords = "krugleshok, " +
+        "krugleshoc, " +
+        "krugleshock, " +
+        "Krugleshock Riza, " +
+        "Mamedow, " +
+        "Mamedov, " +
+        "Riza Mamedow, " +
+        "Riza Mamedov, " +
+        "Portfolio, " +
+        "Портфолио, " +
+        "Риза Мамедов, " +
+        "Риза, " +
+        "Мамедов, " +
+        "Круглешок, " +
+        "Кругляшок"
+
     return (
         <Html>
             <Head>
@@ -11,7 +29,7 @@ export default function Document() {
                       content="Riza Mamedov's portfolio website"
                 />
                 <meta name="robots" content="index, nofollow" />
-                <meta name="keywords" content="Riza Mamedov, Portfolio, Krugleshock" />
+                <meta name="keywords" content={keywords}/>
                 <meta name="author" content="Riza Mamedov" />
                 <link rel="icon" href="/favicon.ico" sizes="any" />
             </Head>
