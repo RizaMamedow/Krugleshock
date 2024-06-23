c5001fac98b0dac4cd48d57e6073628873d7506e
RizaMamedow <riza.mamedow22@gmail.com>
2024-06-23 16:46:43 +0500

    added loading screen

---
+++ src/app/[locale]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="min-h-[50vh] flex items-center justify-center">
+            <h4 className="text-center text-xl">Loading...</h4>
+        </div>
+    )
+}
