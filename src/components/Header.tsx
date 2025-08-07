672370770678f8119291eca2e5e44836893981b5
RizaMamedow <riza.mamedow22@gmail.com>
2025-08-07 09:35:23 +0500

    Fixed an inaccuracy in the layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const playfair = Playfair_Display({
 export default function Header() {
 	const t = useTranslations("Header")
 	return (
-		<div className={clsx(playfair.className, "flex justify-center items-center mt-5 mb-7 flex-col gap-2")}>
+		<div className={clsx(playfair.className, "flex justify-center items-center mt-5 sm:mb-7 flex-col gap-2")}>
 			<h1 className="text-secondary md:text-7xl text-5xl font-black">Krugleshock<span className="text-primary">!</span></h1>
 			<p className="font-regular text-xs text-start text-gray-500">{t("subtitle")}</p>
 		</div>
